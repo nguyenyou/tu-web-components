a9df8950746e2fd4b28e1e46550d1a01ea39b5a5
nguyenyou <tunt081295@gmail.com>
2025-07-06 17:31:09 +0700

    🚢 ship it

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,19 +1,24 @@
-import { LitElement, html } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { LitElement, css, html } from "lit";
+import { customElement } from "lit/decorators.js";
 
-@customElement('tu-button')
+@customElement("tu-button")
 export class TuButton extends LitElement {
+  static styles = css`
+    button {
+      color: green;
+    }
+  `;
   render() {
     return html`
       <button>
         <slot>Button</slot>
       </button>
-    `
+    `;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'tu-button': TuButton
+    "tu-button": TuButton;
   }
 }
