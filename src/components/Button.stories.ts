0e4bdca0d818faa4e30ab7453b59cc66329ca900
nguyenyou <tunt081295@gmail.com>
2025-07-06 17:23:07 +0700

    🚢 ship it

---
+++ src/components/Button.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/web-components-vite';
+import './button.js';
+
+const meta: Meta = {
+    component: 'tu-button',
+    tags: ['autodocs'],
+  };
+  
+  export default meta;
+  
+  type Story = StoryObj;
+  
+  export const Default: Story = {};
