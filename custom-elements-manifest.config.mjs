332d801a88889e70594effcd39e531a5c60708aa
nguyenyou <tunt081295@gmail.com>
2025-07-06 16:23:02 +0700

    🚢 ship it

--- custom-elements-manifest.config.mjs
+++ custom-elements-manifest.config.mjs
@@ -1,8 +1,29 @@
+import { customElementJetBrainsPlugin } from 'custom-element-jet-brains-integration';
+import { customElementVsCodePlugin } from 'custom-element-vs-code-integration';
+
+const outdir = 'dist';
+
 export default {
   // Analyze all TypeScript files in the components folder
   globs: ['src/components/**/*.ts'],
 
   // Enable LitElement support so that the analyzer correctly interprets Lit-specific decorators
   litelement: true,
-  outdir: 'dist',
+  outdir,
+
+  // Generate IDE integration files so that our components have rich IntelliSense support
+  plugins: [
+    // JetBrains IDEs (WebStorm, IntelliJ, etc.)
+    customElementJetBrainsPlugin({
+      outdir,
+      excludeCss: true,
+      packageJson: false,
+    }),
+
+    // Visual Studio Code
+    customElementVsCodePlugin({
+      outdir,
+      cssFileName: null,
+    }),
+  ],
 }; 
