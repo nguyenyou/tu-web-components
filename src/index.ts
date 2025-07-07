655f3b9442532be8772c62087a2bacef47ad5276
nguyenyou <tunt081295@gmail.com>
2025-07-07 20:41:26 +0700

    🚢 ship it

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+import './styles/tu-web-components.css';
 export * from './components/button/button.js';
 export * from './components/badge/badge.js';
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  css: {
+    transformer: 'lightningcss',
+  },
   build: {
+    cssMinify: "lightningcss",
     lib: {
       entry: "src/index.ts",
       name: "TuWebComponents",
